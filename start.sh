77f98594d228615a5d11504562b5be6e3bc672da
scresh <screshowski@gmail.com>
2018-03-20 23:35:38 +0100

    Add passing number of instances as argument

--- start.sh
+++ start.sh
@@ -17,3 +17,17 @@ ControlPort=9051
 TorInstances=10
 InstanceID=0
 
+if [ ${#} -ne 0 ]; then
+	TorInstances=${1}
+fi
+
+while [ ${InstanceID} -lt ${TorInstances} ]; do
+    mkdir -p "tmp/${InstanceID}"
+    tor --SocksPort ${SocksPort} --ControlPort ${ControlPort} --DataDirectory "tmp/${SocksPort}" --quiet &
+
+    let InstanceID=InstanceID+1
+    let SocksPort=SocksPort+2
+    let ControlPort=ControlPort+2
+done
+
+./main.py
