4d800b3c50a164a34601729c33ea9c4f19ce7e7c
Matsyk <matsyk998@gmail.com>
2018-04-06 14:32:29 +0200

    next version

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 from Tkinter import *
-
+import time
 LARGE_FONT = ("Trebuchet MS", 12, "bold italic")
 WHITE = '#FFFFFF'
 SHARK = '#1A1A1D'
@@ -29,7 +29,8 @@ class Application(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.title('Tor Scanner :: Digamma')
-        self.geometry('400x200')
+        self.geometry('600x350')
+        self.resizable(False, False)
         self.configure(background=SHARK)
         self.input()
         self.create_button_1()
@@ -41,7 +42,17 @@ class Application(Tk):
         var = IntVar()
         self.create_radio_button_1()
         self.create_radio_button_2()
+        #zegar
+        self.clock = Label(self, font=('times', 20, 'bold'), bg='green')
+        #self.clock.pack(fill=BOTH, expand=1)
+        self.clock.grid(row=0,column=3)
+        self.tick()
 
+    def tick(self):
+        s = time.strftime('%H:%M:%S')
+        if s != self.clock["text"]:
+            self.clock["text"] = s
+        self.clock.after(200, self.tick)
 
     def print_text(self):
         for x in range(10):
@@ -49,22 +60,25 @@ class Application(Tk):
             self.textbox.insert(END, "\n")
 
     def create_entry1(self):
-        self.entry_1=Entry(self)
-        self.entry_1.pack(side=TOP)
-        #self.entry_1.grid(row=0,column=0)
+        self.entry_1 = Entry(self,width=40,relief=GROOVE )
+
+        #self.entry_1.pack(side=TOP)
+        self.entry_1.grid(row=0,column=0)
 
     def create_entry2(self):
-        self.entry_2 = Entry(self)
-        self.entry_2.pack()
-        #self.entry_2.grid(row=1,column=0)
+        entry_2=Entry_2()
+        #self.entry_2.pack()
+        entry_2.grid(row=1,column=0)
     def input(self):
+        #textbox=Text_1() #funkcja "print" sie jebie bo textbox nie jest globalna,
         self.sb_textbox=Scrollbar(self)
-        self.textbox=Text(self, width=50, height=50)
-        self.textbox.pack(side=BOTTOM)
+        self.textbox=Text(self, width=50, height=15)
+        #self.textbox.pack(side=BOTTOM)
         self.sb_textbox.place(in_=self.textbox, relx=1., rely=0, relheight=1.)
-        self.textbox.place(x=100,y=10)
+        self.textbox.place(x=50,y=100)
         self.textbox.insert(END, "Hello ", ("h1"))
         self.sb_textbox.config(command=self.textbox.yview)
+        self.textbox.grid(row=3,column=0,columnspan =2)
 
     def create_menu(self):
         menu = Menu(self)
@@ -81,20 +95,62 @@ class Application(Tk):
 
     def create_button_1(self):
         button_1 = Button_1('Search',self.print_text)
-        button_1.pack()
-
+        #button_1.pack()
+        button_1.grid(row=0,column=1)
 
     def create_button_2(self):
         button_2=Button_2('stop',backend_function)
-        button_2.pack()
+        #button_2.pack()
+        button_2.grid(row=1,column=1)
 
     def create_radio_button_1(self):
         rd_1=RadioButton_1('opcja',backend_function)
-        rd_1.pack()
+        #rd_1.pack()
+        rd_1.grid(row=0,column=2)
 
     def create_radio_button_2(self):
-        rd_2=RadioButton_1('opcja',backend_function)
-        rd_2.pack()
+        rd_2=RadioButton_1('opcja2',backend_function)
+        #rd_2.pack()
+        rd_2.grid(row=1,column=2)
+
+class Text_1(Text):
+    def __init__(self):
+        Text.__init__(self)
+        self.configure(width=50)
+        self.configure(height=50)
+
+class Entry_1(Entry):
+    def __init__(self):
+        Entry.__init__(self)
+        #self.configure(background =color)
+        #self.configure(borderwidth =) #szerokosc obramowania
+        #self.configure(disabledbackground =text)
+        #self.configure(disabledforeground =)#kolor teksty gdy kontrolka nieaktywna
+        #self.configure(exportselection=)# gdy ustawione jest na True to zaznaczony tekst automatycznie jest kopiowany do systemowego schowka
+        #self.configure(font=font)
+        #self.configure(foreground =)#kolor tekstu
+        #self.configure(justify =)#pozycjonowanie tekstu wewnatrz
+        #self.configure(relief=RAISED) #styl obramowania
+        #self.configure(selectforeground =)#kolor zaznaczaniego teksu
+        #self.configure(state=) #stan kontrolki
+
+class Entry_2(Entry):
+    def __init__(self):
+        Entry.__init__(self)
+        #self.configure(background =color)
+        #self.configure(borderwidth =) #szerokosc obramowania
+        #self.configure(disabledbackground =text)
+        #self.configure(disabledforeground =)#kolor teksty gdy kontrolka nieaktywna
+        #self.configure(exportselection=)# gdy ustawione jest na True to zaznaczony tekst automatycznie jest kopiowany do systemowego schowka
+        #self.configure(font=font)
+        #self.configure(foreground =)#kolor tekstu
+        #self.configure(justify =)#pozycjonowanie tekstu wewnatrz
+        self.configure(relief=RAISED) #styl obramowania
+        #self.configure(selectforeground =)#kolor zaznaczaniego teksu
+        self.configure(state=DISABLED) #stan kontrolki
+        self.configure(width=40)
+
+
 class RadioButton_1(Radiobutton):
     def __init__(self,text,command):
         Radiobutton.__init__(self)
@@ -119,6 +175,7 @@ class Button_1(Button):
         self.configure(relief=GROOVE)
         self.configure(text=text)
         self.configure(padx=20)
+        #self.configure(state=)#stan przyciku
 
 class Button_2(Button):
     def __init__(self, text, command):
@@ -132,7 +189,7 @@ class Button_2(Button):
         self.configure(relief=GROOVE)
         self.configure(text=text)
         self.configure(padx=20)
-
+        #self.configure(state=)#stan przciksu
 
 
 app = Application()
