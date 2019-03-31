9e090d3bcfd55bf49ad8a415a3d1518b60cdd805
scresh <screshowski@gmail.com>
2019-03-31 23:10:25 +0200

    Add IoT API support

--- src/db-api/server.py
+++ src/db-api/server.py
@@ -30,7 +30,7 @@ class Handler(BaseHTTPRequestHandler):
                 pages_dict = {}
 
                 for word in words:
-                    self.cur.execute('SELECT id FROM Words WHERE word == ?;', (word, ))
+                    self.cur.execute('SELECT id FROM Words WHERE word == ?;', (word,))
                     word_id = self.cur.fetchone()
 
                     if word_id:
@@ -39,7 +39,7 @@ class Handler(BaseHTTPRequestHandler):
                     else:
                         continue
 
-                    self.cur.execute('SELECT page_id FROM WordsPages WHERE word_id == ?;', (word_id, ))
+                    self.cur.execute('SELECT page_id FROM WordsPages WHERE word_id == ?;', (word_id,))
                     page_ids = map(lambda x: x[0], self.cur.fetchall())
                     for page_id in page_ids:
                         if page_id not in pages_dict:
@@ -64,8 +64,51 @@ class Handler(BaseHTTPRequestHandler):
 
                 self.send_get_response(response, 200)
 
-        # elif path == '/searchIoT':
-        #     pass
+        elif path == '/searchIoT':
+            if not (0 < len(words) < 20):
+                self.send_get_response({'error': 'Incorrect number of words in query'}, 400)
+
+            elif not all(map(lambda x: 0 < len(x) < 64, words)):
+                self.send_get_response({'error': 'Incorrect word length'}, 400)
+
+            else:
+                device_dict = {}
+
+                for word in words:
+                    self.cur.execute('SELECT id FROM Devices WHERE banner LIKE ? COLLATE NOCASE;', (f'%{word}%',))
+                    device_ids = self.cur.fetchall()
+
+                    if device_ids:
+                        device_ids = [*map(lambda x: x[0], device_ids)]
+                    else:
+                        continue
+
+                    for device_id in device_ids:
+                        if device_id not in device_dict:
+                            device_dict[device_id] = []
+
+                        device_dict[device_id].append(word)
+
+                sorted_devices = [
+                    (device_id, device_dict[device_id]) for device_id in
+                    sorted(device_dict, key=lambda x: len(device_dict[x]), reverse=True)
+                ]
+
+                response = {'results': []}
+                for device in sorted_devices:
+                    device_id = device[0]
+                    words = device[1]
+
+                    self.cur.execute('SELECT socket, banner FROM Devices WHERE id == ?;', (device_id,))
+                    socket, banner = self.cur.fetchone()
+
+                    snippet = generate_snippet(banner, words)
+                    socket_str = socket_to_str(socket)
+
+                    response['results'].append({'socket': socket_str, 'banner': snippet})
+
+                self.send_get_response(response, 200)
+
         else:
             self.send_get_response({'error': 'Page not found'}, 404)
 
@@ -77,6 +120,20 @@ class Handler(BaseHTTPRequestHandler):
         self.wfile.write(
             bytes(json.dumps(content, ensure_ascii=False), 'UTF-8')
         )
+
+
+def socket_to_str(socket):
+    port = socket % 2 ** 16
+    socket = int(socket / 2 ** 16)
+
+    ip_segments = []
+    for i in range(4):
+        ip_segments.append(socket % 256)
+        socket = int(socket / 256)
+
+    ip = '.'.join(map(str, ip_segments))
+
+    return f'{ip}:{port}'
 
 
 def main():
