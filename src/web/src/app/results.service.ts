04e5309d8bb2857177ba520b5d803b0c1e07ede7
scresh <screshowski@gmail.com>
2019-04-01 11:32:21 +0200

    Add IoT results prototype

--- src/web/src/app/results.service.ts
+++ src/web/src/app/results.service.ts
@@ -9,5 +9,10 @@ export class ResultsService {
     let params = new HttpParams().set("query", query);
     return this.http.get("http://127.0.0.1:9000/searchTor", {params: params});
 }
+
+getIoTResults(query: string) {
+    let params = new HttpParams().set("query", query);
+    return this.http.get("http://127.0.0.1:9000/searchIoT", {params: params});
+}
   constructor(private http: HttpClient) { }
 }
